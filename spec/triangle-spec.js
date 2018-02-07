6f91863399b6cbf473d4c677141ba74d9bc8d060
Ross Fletcher <rtbfletch@outlook.com>
2018-02-06 23:21:35 -0800

    not have working karma tests, think it has to do with not having a full gulpfile

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,21 +1,21 @@
 var Triangle = require('./../js/triangle.js').triangleModule;
 
 describe('Triangle', function() {
-  var reusableTriangle;
-
-  beforeEach(function() {
-    reusableTriangle = new Triangle(5, 5, 5);
-  });
-
-  it('should show how beforeEach() works', function() {
-    console.log(reusableTriangle);
-  });
+  // var reusableTriangle;
+  //
+  // beforeEach(function() {
+  //   reusableTriangle = new Triangle(5, 5, 5);
+  // });
+  //
+  // it('should show how beforeEach() works', function() {
+  //   console.log(reusableTriangle);
+  // });
 
   it('should test whether a Triangle has three sides', function() {
-    // var triangle = new Triangle(3,4,5)
-    expect(reusableTriangle.side1).toEqual(5)
-    expect(reusableTriangle.side2).toEqual(5)
-    expect(reusableTriangle.side3).not.toEqual(6)
+    var triangle = new Triangle(3,4,5)
+    expect(triangle.side1).toEqual(3)
+    expect(triangle.side2).toEqual(4)
+    expect(triangle.side3).not.toEqual(6)
   });
 
   it('should correctly determine whether three lengths can be made into a triangle', function() {
@@ -24,10 +24,10 @@ describe('Triangle', function() {
   });
 
   it('should correctly determine whether three lengths are equal', function() {
-    // var triangle = new Triangle(2,2,2)
-    expect(reusableTriangle.side1).toEqual(5)
-    expect(reusableTriangle.side2).toEqual(5)
-    expect(reusableTriangle.side3).toEqual(5)
+    var triangle = new Triangle(2,2,2)
+    expect(triangle.side1).toEqual(2)
+    expect(triangle.side2).toEqual(2)
+    expect(triangle.side3).toEqual(2)
   });
 
 });
